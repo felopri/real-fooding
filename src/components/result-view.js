c162946c4111c3a474be4361e79e2995d8e9f922
Marcos Gil <marcos_gilf@hotmail.com>
2018-06-16 10:36:44 +0200

    Add results icons

--- src/components/result-view.js
+++ src/components/result-view.js
@@ -5,14 +5,52 @@ import { PageViewElement } from './page-view-element';
 import { SharedStyles } from './shared-styles';
 
 class ResultView extends PageViewElement {
-  _render(props) {
+  _render(props, _realFooding, _score) {
     return html`
-      ${SharedStyles}
+       ${SharedStyles}
+      <style>
+        .col {
+          text-align: center;
+        }
+      </style>
       <section>
         <h2>Results</h2>
-        <p>Is Real Food</p>
+        <div class="col">
+          ${this._realFooding !== '0'
+          ? html`<img src="../images/realfood.png" alt="RealFood">`
+          : html`<img src="../images/ultraprocesado.png" alt="Ultraprocesado">`}
+          <h3>${this._score}</h3>
+        </div>
       </section>
     `;
+  }
+
+  static get properties() {
+    return {
+      _realFooding: {
+        type: String,
+        value: ''
+      },
+      _score: {
+        type: String,
+        value: ''
+      }
+    }
+  }
+
+  _firstRendered() {
+    this._getResult();
+  }
+
+  _getResult() {
+    let url = window.location.href;
+    url = url.split('?')[1].split('&');
+    this._realFooding = url[0].split('=')[1];
+    this._score = url[1].split('=')[1];
+  }
+
+  _stateChanged(state) {
+    this._page = state.app.page;
   }
 
 }
