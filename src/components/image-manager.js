60e69850e7c7b5e52f6545f59c2d1f4d83d9f88a
Fernando López <felopri@gmail.com>
2018-06-16 08:26:49 +0200

    Call services

--- src/components/image-manager.js
+++ src/components/image-manager.js
@@ -11,42 +11,27 @@ class ImageManager extends PageViewElement {
       ${SharedStyles}
       <section>
         <h2>Capture your food</h2>
-        <paper-input id="imagesUpload" type="file" on-change="${this._uploadImage}"></paper-input>
+        <paper-input id="imagesUpload" type="file" on-change="${this._uploadImage.bind(this)}"></paper-input>
       </section>
     `;
   }
 
   _uploadImage(event) {
+    this.image = event.currentTarget.inputElement.inputElement.files[0];
     firebase.auth().onAuthStateChanged(user => {
-      this.image = this.inputElement.inputElement.files[0];
       let imageStored = 'images/' + user.uid + '/' + this.image.name;
 
       //Upload image
       var storageRef = firebase.storage().ref(imageStored);
       storageRef.put(this.image).then(() => {
         firebase.storage().ref(imageStored).getDownloadURL().then(url => {
-          //this._computerVision(url);
-          (async () => {
-            const rawResponse = await fetch('https://trackapi.nutritionix.com/v2/natural/nutrients', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'x-app-id': 'e53fecb8',
-                'x-app-key': '4f194e8822b65be0405a2ebb3b065e36',
-                'x-remote-user-id': '0'
-              },
-              body: new FormData({query:'apple'})
-            });
-            const data = await rawResponse.json();
-            console.log(data);
-            //this._isRealFood(data);
-          })();
+          this._computerVision(url);
         });
       });
     });
   }
 
-  /*_computerVision(url) {
+  _computerVision(url) {
     (async () => {
       const rawResponse = await fetch('https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze?visualFeatures=Categories, Tags, Description&language=en', {
         method: 'POST',
@@ -60,27 +45,80 @@ class ImageManager extends PageViewElement {
       const content = await rawResponse.json();
       this._nutritionAPI(content);
     })();
-  }*/
+  }
 
   _nutritionAPI(content) {
+    const tag = content.tags[0].name;
     (async () => {
-      let formData = new FormData();
-      formData.append('query', 'apple');
       const rawResponse = await fetch('https://trackapi.nutritionix.com/v2/natural/nutrients', {
         method: 'POST',
         headers: {
-          'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
-          'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'x-app-id': 'e53fecb8',
+          'x-app-key': '4f194e8822b65be0405a2ebb3b065e36',
+          'x-remote-user-id': '0'
         },
-        body: formData
+        body: JSON.stringify({query: tag})
       });
       const data = await rawResponse.json();
-      this._isRealFood(data);
+      this._isRealFood(data.foods[0]);
     })();
   }
 
-  _isRealFood(data) {
-    console.log(data);
+  _isRealFood(food) {
+    const nutritionValues = this._prepareNutritionData(food),
+      api_key = '9ind3ZwBRx7OOrffFytyblSoJniFoQ0vjlMqUmb+6kU6ZayYsajA0uDIoQb7QqJoZ0JoMRbJyTyqRR9SlFgDNw==';
+    (async () => {
+      const rawResponse = await fetch('https://europewest.services.azureml.net/workspaces/8de399a4e8794319951b59ce9f7cfd3a/services/483c1bc506a248d1a6e98828bee37170/execute?api-version=2.0&format=swagger', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer '+ api_key
+        },
+        body: JSON.stringify(nutritionValues)
+      });
+      const content = await rawResponse.json();
+      console.log('RealFood', content);
+    })();
+  }
+
+  _prepareNutritionData(food){
+    return {
+      "Inputs": {
+        "input1":
+        [
+          {
+              'item_id': "",   
+              'nf_calories': food.nf_calories,   
+              'nf_calories_from_fat': food.nf_calories/2,   
+              'nf_total_fat': food.nf_total_fat,
+              'nf_saturated_fat': food.nf_saturated_fat,   
+              'nf_trans_fatty_acid': food.nf_saturated_fat,   
+              'nf_polyunsaturated_fat': food.nf_saturated_fat,   
+              'nf_monounsaturated_fat': food.nf_saturated_fat,
+              'nf_cholesterol': food.nf_cholesterol, 
+              'nf_sodium': food.nf_sodium,    
+              'nf_total_carbohydrate': food.nf_total_carbohydrate,    
+              'nf_dietary_fiber': food.nf_dietary_fiber,    
+              'nf_sugars': food.nf_sugars,    
+              'nf_protein': food.nf_protein,    
+              'nf_vitamin_a_dv': food.nf_vitamin_a_dv || 0,    
+              'nf_vitamin_c_dv': food.nf_vitamin_c_dv || 0,    
+              'nf_calcium_dv': food.nf_calcium_dv || 0,    
+              'nf_iron_dv': food.nf_iron_dv || 0,    
+              'nf_servings_per_container': '',
+              'serving_qty': food.serving_qty,    
+              'serving_unit': food.serving_unit, 
+              'serving_weight_grams': food.serving_weight_grams,    
+              'real_food': "" 
+              }
+          ]
+        },
+     "GlobalParameters":  {
+      }
+    };
   }
 
 }
