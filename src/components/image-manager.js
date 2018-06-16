02c29913a6b36f5bc82a9b197c116e78740db342
Fernando López <felopri@gmail.com>
2018-06-16 07:23:08 +0200

    Connect APIs

--- src/components/image-manager.js
+++ src/components/image-manager.js
@@ -25,10 +25,62 @@ class ImageManager extends PageViewElement {
       var storageRef = firebase.storage().ref(imageStored);
       storageRef.put(this.image).then(() => {
         firebase.storage().ref(imageStored).getDownloadURL().then(url => {
-          console.log('Image URL:', url);
+          //this._computerVision(url);
+          (async () => {
+            const rawResponse = await fetch('https://trackapi.nutritionix.com/v2/natural/nutrients', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'x-app-id': 'e53fecb8',
+                'x-app-key': '4f194e8822b65be0405a2ebb3b065e36',
+                'x-remote-user-id': '0'
+              },
+              body: new FormData({query:'apple'})
+            });
+            const data = await rawResponse.json();
+            console.log(data);
+            //this._isRealFood(data);
+          })();
         });
       });
     });
+  }
+
+  /*_computerVision(url) {
+    (async () => {
+      const rawResponse = await fetch('https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze?visualFeatures=Categories, Tags, Description&language=en', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Ocp-Apim-Subscription-Key': 'ccbe677fecfe4e9a891b94e6ec6ae286'
+        },
+        body: JSON.stringify({url})
+      });
+      const content = await rawResponse.json();
+      this._nutritionAPI(content);
+    })();
+  }*/
+
+  _nutritionAPI(content) {
+    (async () => {
+      let formData = new FormData();
+      formData.append('query', 'apple');
+      const rawResponse = await fetch('https://trackapi.nutritionix.com/v2/natural/nutrients', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+          'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+        },
+        body: formData
+      });
+      const data = await rawResponse.json();
+      this._isRealFood(data);
+    })();
+  }
+
+  _isRealFood(data) {
+    console.log(data);
   }
 
 }
